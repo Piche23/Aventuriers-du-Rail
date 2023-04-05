e028c664a52d4b4d61f1d1a37e43aa46f50ebf3c
Piche23 <114235332+Piche23@users.noreply.github.com>
2023-04-06 00:33:54 +0200

    Début frontend

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+import flask
+from flask import Flask, redirect
+from database.database import db, init_database
+from database.models import *
+from flask import url_for
+from werkzeug.datastructures import ImmutableMultiDict
 
 app = Flask(__name__)
+url_Hugo = "sqlite:///C:\\Users\\hugop\\OneDrive\\Documents\\Hugo\\Aventuriers du rail\\database\\database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = url_Hugo
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)  # (1) flask prend en compte la base de donnee
+with app.test_request_context():  # (2) bloc exécuté à l'initialisation de Flask
+    init_database()
 
 
+# Joueurs
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/view/joueurs')
+def view_joueurs():
+    joueurs = Joueur.get_all_joueur()
+    return flask.render_template("template_joueurs.html.jinja2", joueurs=joueurs)
+
 
 
 if __name__ == '__main__':
