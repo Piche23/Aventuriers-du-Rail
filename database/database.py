6bb572710c8dc38da6d0504ba4385605e0a3049a
Piche23 <114235332+Piche23@users.noreply.github.com>
2023-04-06 00:07:08 +0200

    Modifications initialisation de la base de données

---
+++ database/database.py
@@ -0,0 +1,36 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def init_database():
+    """
+    Initialise la base de données en supprimant toutes les tables existantes,
+    en créant toutes les tables et en appelant la fonction populate_database
+    pour ajouter des données à la base de données.
+    """
+    db.drop_all()
+    db.create_all()
+    populate_database()
+
+
+# Fonction pour peupler la base de données
+def populate_database():
+    # Import des modèles de la base de données
+    from models import Joueur,Plateau, Partie, Score
+
+    # Ajout des joueurs
+    Joueur.ajouter_joueur("Hugo")
+    Joueur.ajouter_joueur("Maman")
+    Joueur.ajouter_joueur("Papa")
+    Joueur.ajouter_joueur("Débo")
+    Joueur.ajouter_joueur("Miguel")
+
+    # Ajout des plateaux
+    Plateau.ajouter_plateau("Europe")
+    Plateau.ajouter_plateau("Afrique")
+    Plateau.ajouter_plateau("Pays-Bas")
+    Plateau.ajouter_plateau("Suisse")
+
+    # Ajout des parties
+
+
